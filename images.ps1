7bac9b46b84cd81ffc519c8e89b13a94155e377c
Jonathan Nicholas <jonathan.nicholas@protonmail.com>
2024-09-29 16:58:23 +1000

    fix: image script

--- images.ps1
+++ images.ps1
@@ -17,40 +17,25 @@ $systemLists = (Invoke-WebRequest -URI https://raw.githubusercontent.com/Mustard
 foreach ($xmlFile in $xmlFiles) {
     # Extract the system name from the file path
     # This will put things in the muOS directory structure according to current catalogue structure
-    if ( $systemLists.containsKey([System.IO.Path]::GetFileNameWithoutExtension($xmlFile.Directory.Name)))
+    $folderName = [System.IO.Path]::GetFileNameWithoutExtension($xmlFile.Directory.Name)
+    if ( $systemLists.containsKey($systemName))
     {
-        $systemName = $systemLists[[System.IO.Path]::GetFileNameWithoutExtension($xmlFile.Directory.Name)]
+        $muosSystemFile = $systemLists[[System.IO.Path]::GetFileNameWithoutExtension($systemName)]
+        $systemName = $folderName
     } else {
-        $systemName = "arcade.ini" # hardcoded since TBS GO EXTRA have weird folders for arcade games
+        $muosSystemFile = "arcade.ini" # hardcoded since TBS GO EXTRA have weird folders for arcade games
+        $systemName = "ARCADE"
     }
 
     # use muOS assign content to set system name
-    $systemName = (Invoke-WebRequest -URI https://raw.githubusercontent.com/MustardOS/internal/refs/heads/main/init/MUOS/info/assign/$systemName).Content | Where-Object { $_ -match 'catalogue=' }
-    $systemName = $systemName.Split('=')[1]
+    $muosSystemName = (Invoke-WebRequest -URI https://raw.githubusercontent.com/MustardOS/internal/refs/heads/main/init/MUOS/info/assign/$muosSystemFile).Content | Where-Object { $_ -match 'catalogue=' }
+    $muosSystemName = $muosSystemFile.Split('=')[1]
 
-    <# foreach ($game in $xml.SelectNodes("//game")) {
-        $gamePath = $game.SelectSingleNode("path").InnerText
-        $gameDesc = $game.SelectSingleNode("desc").InnerText #-replace "`r|`n", " " # Early versions stripped linebreaks
-        $gameGenre = $game.SelectSingleNode("genre").InnerText
-
-        # Extracting filename from the path
-        # This ensures the metadata get's the correct name /INFO/content/System/meta/filename.txt
-        $fileName = [System.IO.Path]::GetFileNameWithoutExtension($gamePath)
-    
-        # Created the files!
-        $outputFolderPath = "$muosInfo\$systemName\text\"
-        $outputFilePath = Join-Path -Path $outputFolderPath -ChildPath "$fileName.txt"
-
-        # Create the directories if needed
-        if (-not (Test-Path -LiteralPath $outputFolderPath)) {
-            New-Item -ItemType Directory -Path $outputFolderPath | Out-Null
-        }
-        #$content = "$descText`r`ndesc=$gameDesc`r`n`r`n$kindText`r`nkind=$gameGenre`r`n`r`n$timeText`r`n`r`n$coreText`r`n`r`n$modeText"
-        $content = $gameDesc
-        Write-Host "Writing $outputFilePath"
-        Set-Content -LiteralPath $outputFilePath -Value $content
-    } #>
-
-    Copy-Item -Path "${xmlFile.Directory}\box" -Destination "$muosInfo\$systemName\box"
-    Copy-Item -Path "${xmlFile.Directory}\preview" -Destination "$muosInfo\$systemName\preview"
+    Copy-Item -Path "${xmlFile.Directory}\box" -Destination "$muosInfo\$muosSystemFile\box"
+    Copy-Item -Path "${xmlFile.Directory}\preview" -Destination "$muosInfo\$muosSystemFile\preview"
+    Invoke-WebRequest "https://raw.githubusercontent.com/Vidnez/retro-systems-icons-for-GarlicOS/refs/heads/master/system/$systemName.png" -OutFile "$muosInfo\Folder\box\$folderName.png"
 }
+
+# add portmaster icon for reproducibility
+
+Invoke-WebRequest "https://raw.githubusercontent.com/Vidnez/retro-systems-icons-for-GarlicOS/refs/heads/master/system/PORTS.png" -OutFile "$muosInfo\Folder\box\PORTS.png"
